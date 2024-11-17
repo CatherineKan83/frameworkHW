da24d47a685c2f77949565cba832c282f4a04705
CatherineKan83 <kankatya@bk.ru>
2024-11-18 02:57:48 +0500

    add

---
+++ HW3/src/main/HW3/services/NotificationService.java
@@ -0,0 +1,19 @@
+package main.HW3.services;
+
+import main.HW3.domain.User;
+
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+
+    public void notifyUser(User user) {
+        System.out.println("A new user has been created: " + user.getName());
+    }
+
+    public void sendNotification(String s) {
+        System.out.println(s);
+    }
+}
---
+++ HW3/src/test/java/HW3/Hw3ApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.HW3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHw3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
